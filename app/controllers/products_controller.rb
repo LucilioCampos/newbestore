42cb3e5ded77125a754cacd28517751008752d3f
lucilio <lucilio@codeby.com.br>
2019-10-27 23:34:43 -0300

    removed required

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def product_params
-      params.require(:product).permit(:name, :image, :description, :price)
+      params.permit(:name, :image, :description, :price)
     end
 end
